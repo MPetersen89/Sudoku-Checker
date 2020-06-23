5a597f589eccabe01f55f53fa69059b526a4ac03
Michael Petersen <michael_petersen_email@yahoo.com>
2020-06-23 14:55:53 -0700

    create test that verifies that each row contains integers 1-9.
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>
    Co-authored-by: Janet Karpenske<janetkarpenske@gmail.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -8,8 +8,20 @@ export function Row(input1, input2, input3, input4, input5, input6, input7, inpu
   this.input7 = input7;
   this.input8 = input8;
   this.input9 = input9;
-};
+}
 
 Row.prototype.rowCheck = function() {
-
-}
+  let rowArray = [];
+  rowArray.push(this.input1, this.input2, this.input3, this.input4, this.input5, this.input6, this.input7, this.input8, this.input9);
+  
+  for (let i = 0; i < rowArray.length; i++) {
+    for (let j = i + 1; j < rowArray.length; j++) {
+      if (rowArray[i] === (rowArray[j])) {
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
+  }
+};
